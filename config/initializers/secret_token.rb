fab6d5550bd627027ee0f6501fd547aea139bb10
Bryan Cannoy <notbryancannoy@gmail.com>
2014-08-19 20:40:41 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DndCc::Application.config.secret_key_base = '2b4e5366a3d5c589a54ba0c4f40d515cc32fee08b46753dee29e977443f66b6f95066e558658342a7ef78828930d406f63400f719e02aae62d523ad95fbd618b'
